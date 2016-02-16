a833ed177b19f1573928ed380a423111c2938701
ana cecilia <ana.cecilia.alvarez.ortiz@gmail.com>
2016-02-16 13:12:19 -0500

    Update magicalbike.js

--- html/ana/magicalbike.js
+++ html/ana/magicalbike.js
@@ -2,11 +2,11 @@ var draw_bike = function(position) {
 	if (position < 80) {
 		return placeImage(bike, emptyScene(400, 100), position, 30);
 	} else if (position < 160) {
-		return placeImage(bike, emptyScene(400, 100), position, 100);
+		return placeImage(bike, emptyScene(400, 100), position, 10);
 	} else if (position < 320){
 		return placeImage(bike, emptyScene(400, 100), position, 30);
-	}else {
-		return placeImage(bike, emptyScene(400, 100), 400, 100);
+	} else {
+		return placeImage(bike, emptyScene(400, 100), 320, 30);
 	}
 };
 
