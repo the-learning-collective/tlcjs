cb81b002e7b378c25ff807d33b3ceebe93810059
ana cecilia <ana.cecilia.alvarez.ortiz@gmail.com>
2016-02-18 20:53:02 -0500

    Update steviechubs.js

--- html/ana/steviechubs.js
+++ html/ana/steviechubs.js
@@ -1,3 +1,4 @@
+//this is building stevies chubby face by placing several circles over each other
 var faceEar = placeImage(circle(30, "orange"), circle(100,"orange"), 0 , 0);
 var face = placeImage(circle(30, "orange"), faceEar, 140, 0);
 var LeftEye = placeImage(circle(15, "white"), face, 45, 55);
@@ -6,9 +7,31 @@ var LpinkEar = placeImage(circle(15, "pink"), WithEyes, 13, 10);
 var RpinkEar = placeImage(circle(15, "pink"), LpinkEar, 160, 10);
 var LEyeball = placeImage(circle(7, "black"), RpinkEar, 53, 63);
 var REyeball = placeImage(circle(7, "black"), LEyeball, 133, 63);
-var LCheek = placeImage(circle(17, "white"), REyeball, 68, 115);
-var RCheek = placeImage(circle(17, "white"), LCheek, 103, 115);
-var head = placeImage(circle(15, "pink"), RCheek, 87, 97);
-var belly = overlay(circle(70, "yellow"), circle(125, "orange"));
+var belly = circle(125, "orange");
+var scene = emptyScene(300, 300);
+var chubbyhead = placeImage(REyeball, belly, 40, 0);
+var chubbyscene = overlay(chubbyhead, scene);
 
-draw(placeImage(head, belly, 40, 0));
+
+//with animate, my input will always be time, and the output will always be an image
+// time -> image
+//we are going to try to have stevie give us a licky-kiss
+var lick = function(time) {
+	var remainder = (time % 40);
+	if (remainder < 10) {
+		var tongue = placeImage(circle(15, "pink"), chubbyscene, 150, remainder + 153);
+	} else  {
+		var tongue = placeImage(circle(15, "pink"), chubbyscene, 150, 153);			
+	}
+	var LCheek = placeImage(circle(17, "white"), tongue, 133, 142);
+	var RCheek = placeImage(circle(17, "white"), LCheek, 165, 142);
+	var head = placeImage(circle(15, "pink"), RCheek, 150, 130);
+	return head
+};
+
+animate(lick);
+
+
+
+
+
