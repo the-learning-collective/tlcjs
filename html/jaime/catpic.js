c35c038fb6ed6cf5abb514fddbfcba5eeb9c71e3
rowmyboat <lionmutt@gmail.com>
2016-03-01 21:35:33 -0500

    fixed the ears

--- html/jaime/catpic.js
+++ html/jaime/catpic.js
@@ -1,21 +1,26 @@
-var background = rectangle(400, 400, "orange");
+var background = rectangle(420, 420, "orange");
+var scene = placeImage(background, emptyScene(420, 420), 0, 0);
 var body = circle(140, "white");
-var scene = placeImage(background, emptyScene(401, 401), 0, 0);
-var head = circle(90, "white")
-var eye = circle(10, "green");
+var leftOutterear = circle(26, "white");
+var rightOutterear = circle(26, "white");
 var leftEar = circle(20, "pink");
 var rightEar = circle(20, "pink");
+var head = circle(90, "white")
+var eye = circle(10, "green");
 var nose = circle(13, "pink");
 var nose = circle(15, "pink");
 var spot1 = circle(25, "black");
 var spot2 = circle(25, "black");
 var spot3 = circle(25, "black");
+
 var body1 = (overlay(body, scene));
-var head1 = (placeImage(head, body1, 225, 25));
-var eye1 = (placeImage(eye, head1, 287, 58));
-var ear1 = (placeImage(leftEar, eye1, 270, 5));
+var outterEar1 = (placeImage(leftOutterear, body1, 265, 3));
+var outterEar2 = (placeImage(rightOutterear, outterEar1, 324, 3));
+var ear1 = (placeImage(leftEar, outterEar2, 270,5));
 var ear2 = (placeImage(rightEar, ear1, 330, 5));
-var nose1 = (placeImage(nose, ear2, 310, 92));
+var head1 = (placeImage(head, ear2, 225, 25));
+var eye1 = (placeImage(eye, head1, 287, 58));
+var nose1 = (placeImage(nose, eye1, 310, 92));
 var spot11 = (placeImage(spot1, nose1, 102, 132));
 var spot22 = (placeImage(spot2, spot11, 155, 100));
 
