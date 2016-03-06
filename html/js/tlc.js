39c235e06d6449aec845a9adee5d8c13ccfa441a
Libby H <libby@daydrea.me>
2016-03-06 15:36:42 -0500

    Add array type, object equality test, and array functions

--- html/js/tlc.js
+++ html/js/tlc.js
@@ -11,7 +11,7 @@ function test(desc, expected, given) {
   }
   try {
     var res = given();
-    if (expected !== res) {
+    if (!(_.isEqual(expected, res))) {
       err(res);
     }
   } catch (e) {
@@ -85,6 +85,7 @@ function shouldEqual(given, expected) {
 var tNumber = "number";
 var tString = "string";
 var tObject = "object";
+var tArray = "array";
 var tBoolean = "boolean";
 var tFunction = "function";
 var tAny = "anything";
@@ -110,6 +111,12 @@ function __type(ty, err, val) {
       } else {
         return val;
       }
+    } else if (ty === tArray) {
+        if (_.isArray(val)) {
+            return val;
+        } else {
+            throw new TypeError(err);
+        }
     } else if (typeof val !== ty) {
       console.log("Value " + val + " has wrong type. Expected " + ty + " but got " + typeof val);
       throw new TypeError(err);
@@ -146,6 +153,9 @@ test("__type numbers", 10, function () { return __type(tNumber, "", 10); });
 test("__type strings", "foo", function () { return __type(tString, "", "foo"); });
 testRaises("__type numbers", function () { return __type(tNumber, "", "hello"); });
 testRaises("__type tAny doesn't match undefined", function () { return __type(tAny, "", undefined); });
+test("__type arrays", [1], function() { return __type(tArray, "", [1]); });
+testRaises("__type arrays don't match objects", function () { return __type(tArray, "", {}); });
+testRaises("__type objects don't match arrays", {}, function () { return __type(tObject, "", []); });
 testRaises("__type higher order arity", function () {
   return __type(tArrow([tNumber], tAny), "", function () {})();
 });
@@ -540,17 +550,52 @@ var width = _type([tObject], tNumber, width_usage, function(image) {
   return image.width;
 });
 
+var bike = circle(30, "red");
+
 var height_usage = "height(): Requires one argument, an image. For example: height(circle(20, 'red')).";
 var height = _type([tObject], tNumber, height_usage, function(image) {
   return image.height;
 });
 
-var stringLength_usage = "length(): Requires one argument, a string. For example: length('Hello world')."
+var stringLength_usage = "stringLength(): Requires one argument, a string. For example: length('Hello world')."
 var stringLength = _type([tString], tNumber, stringLength_usage, function(string) {
   return string.length;
 });
 
-var bike = circle(30, "red");
+/* Array functions */
+
+var link_usage = "link(): Requires two arguments, an item to add to the beginning an array, and an array. For example: link(1, [2,3,4,5])."
+var link = _type([tAny, tArray], tArray, link_usage, function(item, array) {
+    var newArray = _.cloneDeep(array);
+    newArray.unshift(item);
+    return newArray;
+});
+test ("link add to empty", [1], function () { return link(1, []); });
+test ("link add to existing", [1,2], function () { return link (1, [2]); });
+
+var isEmpty_usage = "isEmpty(): Requires one argument, an array. For example: isEmpty([])."
+var isEmpty = _type([tArray], tBoolean, isEmpty_usage, function(array) {
+    return array.length === 0;
+});
+test("isEmpty empty list", true, function () { return isEmpty([]); });
+test("isEmpty non-empty", false, function () { return isEmpty([1]); });
+
+var head_usage = "head(): Requires one argument, an array. For example: head([1,2,3])."
+var head = _type([tArray], tAny, head_usage, function(array) {
+    return _.head(array);
+});
+testRaises("don't call head on empty arrays", function () { return head([]); });
+test("head non-empty", 1, function () { return head([1]); });
+test("head non-empty longer", 1, function () { return head([1,2,3]); });
+
+var tail_usage = "tail(): Requires one argument, an array. For example: tail([1,2,3])."
+var tail = _type([tArray], tAny, tail_usage, function(array) {
+    return _.tail(array);
+});
+// tail of an empty list is an empty list??
+test("tail empty", [], function () { return tail([]); });
+test("tail non-empty", [], function () { return tail([1]); });
+test("tail non-empty longer", [2,3], function () { return tail([1,2,3]); });
 
 /* Incorporating into EJS sandbox */
 function sandbox_draw(win, image) {
